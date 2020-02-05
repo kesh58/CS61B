f614c104c2ebeda7631d1c090b1dc711e47e7640
Ke Shang <shang.58@osu.edu>
2020-02-05 12:19:16 -0500

    add lab9

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,12 +1,14 @@
 package lab9;
 
+import com.sun.jdi.Value;
+
 import java.util.Iterator;
 import java.util.Set;
 
 /**
  * Implementation of interface Map61B with BST as core data structure.
  *
- * @author Your name here
+ * @author Ke Shang
  */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -44,7 +46,15 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      *  or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if(p == null){
+            return null;
+        }else if(key.compareTo(p.key)>0){
+            return getHelper(key, p.right);
+        }else if(key.compareTo(p.key)<0){
+            return getHelper(key, p.left);
+        }else{
+            return p.value;
+        }
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
@@ -52,14 +62,22 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
     /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
       * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if(p == null){
+            size +=1;
+            return new Node(key, value);
+        }else if(key.compareTo(p.key)>0){
+            p.right = putHelper(key, value, p.right);
+        }else if(key.compareTo(p.key)< 0){
+            p.left =  putHelper(key, value, p.left);
+        }
+        return p;
     }
 
     /** Inserts the key KEY
@@ -67,13 +85,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -81,16 +99,88 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
+
         throw new UnsupportedOperationException();
     }
 
+    public Node Node_position(K key, Node p){
+        if(p == null){
+            return null;
+        }else if(key.compareTo(p.key)>0){
+            if(p.right.key == key){
+                return p;
+            }else {
+                return Node_position(key, p.right);
+            }
+        }else if(key.compareTo(p.key)<0){
+            if(p.left.key == key){
+                return p;
+            }else {
+                return Node_position(key, p.left);
+            }
+        }else{
+            return p;
+        }
+    }
+
+    /*find the min node of the BST */
+    public Node min(Node p){
+        if(p.left == null){
+            return p;
+        }
+        return min(p.left);
+    }
+
+    public Node delete_min(Node p){
+        if(p.left == null){
+            return p.right;
+        }
+        p.left = delete_min(p.left);
+        size -=1;
+        return p;
+    }
     /** Removes KEY from the tree if present
      *  returns VALUE removed,
      *  null on failed removal.
      */
+
+    public Node remove(K key, Node p) {
+        if(p == null){
+            return null;
+        }
+        if(p.key.compareTo(key)>0){
+            p.left = remove(key, p.left);
+        } else if(p.key.compareTo(key) <0){
+            p.right = remove(key, p.right);
+        } else {
+
+            if(p.left == null){
+                size -=1;
+                return p.right;
+            } else if(p.right == null){
+                size -=1;
+                return p.left;
+            } else{
+                Node temp = min(p.right);
+
+                p.right = delete_min(p.right);
+                temp.right = p.right;
+                temp.left = p.left;
+                p = temp;
+
+                return temp;
+            }
+        }
+        return p;
+    }
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V value = get(key);
+        if(value != null){
+            root = remove(key, root);
+        }
+        return value;
+
     }
 
     /** Removes the key-value entry for the specified key only if it is
@@ -105,5 +195,15 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     @Override
     public Iterator<K> iterator() {
         throw new UnsupportedOperationException();
+    }
+    public static void main(String[] args) {
+        BSTMap<String, Integer> bstmap = new BSTMap<>();
+        bstmap.put("hello", 5);
+        bstmap.put("cat", 10);
+        bstmap.put("fish", 22);
+        bstmap.put("zebra", 90);
+        bstmap.put("break", 50);
+        bstmap.put("great", 20);
+        int x = bstmap.remove("cat");
     }
 }
--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -39,13 +39,12 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      *  computing the hashcode, followed by modding by the number of buckets.
      *  To handle negative numbers properly, uses floorMod instead of %.
      */
-    private int hash(K key) {
+    private int hash(K key, int z) {
         if (key == null) {
             return 0;
         }
 
-        int numBuckets = buckets.length;
-        return Math.floorMod(key.hashCode(), numBuckets);
+        return Math.floorMod(key.hashCode(), z);
     }
 
     /* Returns the value to which the specified key is mapped, or null if this
@@ -53,19 +52,48 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        int hash_code = hash(key, buckets.length);
+        return buckets[hash_code].get(key);
     }
 
+    private ArrayMap<K, V>[] resize(ArrayMap<K, V>[] bkt, int size){
+        ArrayMap<K, V>[] r = new ArrayMap[size];
+
+        for (ArrayMap<K,V> s : bkt){
+            if(s!= null){
+                for(K item : s.keySet()){
+                    int new_hash = hash(item, r.length);
+                    int old_hash = hash(item, bkt.length);
+                    V value = bkt[old_hash].get(item);
+                    r[new_hash] = new ArrayMap<>();
+                    r[new_hash].put(item, value);
+                }
+            }
+        }
+        return r;
+    }
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        int hash_code = hash(key, buckets.length);
+        if(buckets[hash_code] == null){
+            buckets[hash_code] = new ArrayMap<>();
+        }
+        if(!buckets[hash_code].containsKey(key)){
+            size +=1;
+        }
+        buckets[hash_code].put(key, value);
+
+        if(loadFactor()> MAX_LF){
+            buckets = resize(buckets, buckets.length*2);
+        }
+
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
